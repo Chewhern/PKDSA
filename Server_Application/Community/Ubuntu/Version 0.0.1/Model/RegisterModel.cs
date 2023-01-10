532828a75c328a2e49fa744a6dce064deae9909a
Chrono <chrono_legionnaire@protonmail.com>
2023-01-11 00:40:06 +0800

    Add files via upload

---
+++ Server_Application/Community/Ubuntu/Version 0.0.1/Model/RegisterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PKDSA_ServerApp.Model
+{
+    public class RegisterModel
+    {
+        public String UserIDCount { get; set; }
+        public String UserIDChecker { get; set; }
+        public String Status { get; set; }
+    }
+}
