9a952f16919b3e8eb809be98d62efd93eb1a08de
Chrono <chrono_legionnaire@protonmail.com>
2023-04-21 18:00:59 +0800

    Add files via upload

---
+++ Management_Applications/Community/Windows/PKDSA_UMWinClient/Version 0.0.2/LoginUsingSubKeys.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace PKDSA_UMWinClient
+{
+    partial class LoginUsingSubKeys
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StatusTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SubKeyIdentifierTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UserIDTB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.UserSignedChallengeTB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ServerChallengeTB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.RequestChallengeBTN = new System.Windows.Forms.Button();
+            this.LoginBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StatusTB
+            // 
+            this.StatusTB.Location = new System.Drawing.Point(393, 141);
+            this.StatusTB.Multiline = true;
+            this.StatusTB.Name = "StatusTB";
+            this.StatusTB.ReadOnly = true;
+            this.StatusTB.Size = new System.Drawing.Size(250, 69);
+            this.StatusTB.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(393, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Status";
+            // 
+            // SubKeyIdentifierTB
+            // 
+            this.SubKeyIdentifierTB.Location = new System.Drawing.Point(12, 141);
+            this.SubKeyIdentifierTB.Multiline = true;
+            this.SubKeyIdentifierTB.Name = "SubKeyIdentifierTB";
+            this.SubKeyIdentifierTB.Size = new System.Drawing.Size(250, 69);
+            this.SubKeyIdentifierTB.TabIndex = 25;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Sub Key Identifier";
+            // 
+            // UserIDTB
+            // 
+            this.UserIDTB.Location = new System.Drawing.Point(12, 34);
+            this.UserIDTB.Multiline = true;
+            this.UserIDTB.Name = "UserIDTB";
+            this.UserIDTB.ReadOnly = true;
+            this.UserIDTB.Size = new System.Drawing.Size(250, 69);
+            this.UserIDTB.TabIndex = 23;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 20);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Your User ID";
+            // 
+            // UserSignedChallengeTB
+            // 
+            this.UserSignedChallengeTB.Location = new System.Drawing.Point(393, 34);
+            this.UserSignedChallengeTB.Multiline = true;
+            this.UserSignedChallengeTB.Name = "UserSignedChallengeTB";
+            this.UserSignedChallengeTB.Size = new System.Drawing.Size(250, 69);
+            this.UserSignedChallengeTB.TabIndex = 30;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(393, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 20);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "User Signed Challenge";
+            // 
+            // ServerChallengeTB
+            // 
+            this.ServerChallengeTB.Location = new System.Drawing.Point(12, 251);
+            this.ServerChallengeTB.Multiline = true;
+            this.ServerChallengeTB.Name = "ServerChallengeTB";
+            this.ServerChallengeTB.ReadOnly = true;
+            this.ServerChallengeTB.Size = new System.Drawing.Size(250, 69);
+            this.ServerChallengeTB.TabIndex = 32;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 20);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Server\'s Challenge";
+            // 
+            // RequestChallengeBTN
+            // 
+            this.RequestChallengeBTN.Location = new System.Drawing.Point(12, 330);
+            this.RequestChallengeBTN.Name = "RequestChallengeBTN";
+            this.RequestChallengeBTN.Size = new System.Drawing.Size(250, 64);
+            this.RequestChallengeBTN.TabIndex = 33;
+            this.RequestChallengeBTN.Text = "Request Challenge";
+            this.RequestChallengeBTN.UseVisualStyleBackColor = true;
+            this.RequestChallengeBTN.Click += new System.EventHandler(this.RequestChallengeBTN_Click);
+            // 
+            // LoginBTN
+            // 
+            this.LoginBTN.Location = new System.Drawing.Point(393, 228);
+            this.LoginBTN.Name = "LoginBTN";
+            this.LoginBTN.Size = new System.Drawing.Size(250, 64);
+            this.LoginBTN.TabIndex = 34;
+            this.LoginBTN.Text = "Login using sub keys";
+            this.LoginBTN.UseVisualStyleBackColor = true;
+            this.LoginBTN.Click += new System.EventHandler(this.LoginBTN_Click);
+            // 
+            // LoginUsingSubKeys
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 556);
+            this.Controls.Add(this.LoginBTN);
+            this.Controls.Add(this.RequestChallengeBTN);
+            this.Controls.Add(this.ServerChallengeTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.UserSignedChallengeTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.StatusTB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SubKeyIdentifierTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UserIDTB);
+            this.Controls.Add(this.label5);
+            this.Name = "LoginUsingSubKeys";
+            this.Text = "LoginUsingSubKeys";
+            this.FormClosing += new FormClosingEventHandler(OnFormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox StatusTB;
+        private Label label1;
+        private TextBox SubKeyIdentifierTB;
+        private Label label2;
+        private TextBox UserIDTB;
+        private Label label5;
+        private TextBox UserSignedChallengeTB;
+        private Label label3;
+        private TextBox ServerChallengeTB;
+        private Label label4;
+        private Button RequestChallengeBTN;
+        private Button LoginBTN;
+    }
+}
---
+++ Management_Applications/Community/Windows/PKDSA_UMWinClient/Version 0.0.2/Menu.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PKDSA_UMWinClient
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.IPConfigBTN = new System.Windows.Forms.Button();
+            this.RegistrationBTN = new System.Windows.Forms.Button();
+            this.ChangeMasterKeyBTN = new System.Windows.Forms.Button();
+            this.AddRemoveKeyIdentifierBTN = new System.Windows.Forms.Button();
+            this.AddChangeSubKeysBTN = new System.Windows.Forms.Button();
+            this.UserLoginBTN = new System.Windows.Forms.Button();
+            this.TORBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(188, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(418, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please choose one of the following actions";
+            // 
+            // IPConfigBTN
+            // 
+            this.IPConfigBTN.Location = new System.Drawing.Point(228, 54);
+            this.IPConfigBTN.Name = "IPConfigBTN";
+            this.IPConfigBTN.Size = new System.Drawing.Size(334, 56);
+            this.IPConfigBTN.TabIndex = 1;
+            this.IPConfigBTN.Text = "1. Server\'s IP address configuration";
+            this.IPConfigBTN.UseVisualStyleBackColor = true;
+            this.IPConfigBTN.Click += new System.EventHandler(this.IPConfigBTN_Click);
+            // 
+            // RegistrationBTN
+            // 
+            this.RegistrationBTN.Location = new System.Drawing.Point(228, 199);
+            this.RegistrationBTN.Name = "RegistrationBTN";
+            this.RegistrationBTN.Size = new System.Drawing.Size(334, 56);
+            this.RegistrationBTN.TabIndex = 2;
+            this.RegistrationBTN.Text = "2. Register an account";
+            this.RegistrationBTN.UseVisualStyleBackColor = true;
+            this.RegistrationBTN.Click += new System.EventHandler(this.RegistrationBTN_Click);
+            // 
+            // ChangeMasterKeyBTN
+            // 
+            this.ChangeMasterKeyBTN.Location = new System.Drawing.Point(228, 271);
+            this.ChangeMasterKeyBTN.Name = "ChangeMasterKeyBTN";
+            this.ChangeMasterKeyBTN.Size = new System.Drawing.Size(334, 56);
+            this.ChangeMasterKeyBTN.TabIndex = 3;
+            this.ChangeMasterKeyBTN.Text = "3. Change Master Keys";
+            this.ChangeMasterKeyBTN.UseVisualStyleBackColor = true;
+            this.ChangeMasterKeyBTN.Click += new System.EventHandler(this.ChangeMasterKeyBTN_Click);
+            // 
+            // AddRemoveKeyIdentifierBTN
+            // 
+            this.AddRemoveKeyIdentifierBTN.Location = new System.Drawing.Point(228, 344);
+            this.AddRemoveKeyIdentifierBTN.Name = "AddRemoveKeyIdentifierBTN";
+            this.AddRemoveKeyIdentifierBTN.Size = new System.Drawing.Size(334, 56);
+            this.AddRemoveKeyIdentifierBTN.TabIndex = 4;
+            this.AddRemoveKeyIdentifierBTN.Text = "3. Add or remove sub key identifiers";
+            this.AddRemoveKeyIdentifierBTN.UseVisualStyleBackColor = true;
+            this.AddRemoveKeyIdentifierBTN.Click += new System.EventHandler(this.AddRemoveKeyIdentifierBTN_Click);
+            // 
+            // AddChangeSubKeysBTN
+            // 
+            this.AddChangeSubKeysBTN.Location = new System.Drawing.Point(228, 415);
+            this.AddChangeSubKeysBTN.Name = "AddChangeSubKeysBTN";
+            this.AddChangeSubKeysBTN.Size = new System.Drawing.Size(334, 56);
+            this.AddChangeSubKeysBTN.TabIndex = 5;
+            this.AddChangeSubKeysBTN.Text = "4. Add or change sub keys";
+            this.AddChangeSubKeysBTN.UseVisualStyleBackColor = true;
+            this.AddChangeSubKeysBTN.Click += new System.EventHandler(this.AddChangeSubKeysBTN_Click);
+            // 
+            // UserLoginBTN
+            // 
+            this.UserLoginBTN.Location = new System.Drawing.Point(228, 489);
+            this.UserLoginBTN.Name = "UserLoginBTN";
+            this.UserLoginBTN.Size = new System.Drawing.Size(334, 56);
+            this.UserLoginBTN.TabIndex = 6;
+            this.UserLoginBTN.Text = "5. User login using sub keys";
+            this.UserLoginBTN.UseVisualStyleBackColor = true;
+            this.UserLoginBTN.Click += new System.EventHandler(this.UserLoginBTN_Click);
+            // 
+            // TORBTN
+            // 
+            this.TORBTN.Location = new System.Drawing.Point(228, 126);
+            this.TORBTN.Name = "TORBTN";
+            this.TORBTN.Size = new System.Drawing.Size(334, 56);
+            this.TORBTN.TabIndex = 7;
+            this.TORBTN.Text = "1. Download and start TOR deepweb";
+            this.TORBTN.UseVisualStyleBackColor = true;
+            this.TORBTN.Click += new System.EventHandler(this.TORBTN_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 555);
+            this.Controls.Add(this.TORBTN);
+            this.Controls.Add(this.UserLoginBTN);
+            this.Controls.Add(this.AddChangeSubKeysBTN);
+            this.Controls.Add(this.AddRemoveKeyIdentifierBTN);
+            this.Controls.Add(this.ChangeMasterKeyBTN);
+            this.Controls.Add(this.RegistrationBTN);
+            this.Controls.Add(this.IPConfigBTN);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Menu";
+            this.Text = "Public Key Digital Signature Authentication User Information Managing Menu";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button IPConfigBTN;
+        private Button RegistrationBTN;
+        private Button ChangeMasterKeyBTN;
+        private Button AddRemoveKeyIdentifierBTN;
+        private Button AddChangeSubKeysBTN;
+        private Button UserLoginBTN;
+        private Button TORBTN;
+    }
+}
