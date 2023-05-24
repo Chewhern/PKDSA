a288af2a422c09cf42671c48f5b09b641b7dc556
Chrono <chrono_legionnaire@protonmail.com>
2023-05-24 14:05:17 +0800

    Add files via upload

---
+++ Server_Application/Community/Ubuntu/Version 0.0.3/Helper/HexaChecker.cs
@@ -0,0 +1,20 @@
+﻿using ASodium;
+namespace PKDSA_ServerApp.Helper
+{
+    public static class HexaChecker
+    {
+        public static Boolean CheckHexa(String Data) 
+        {
+            Boolean CheckBool = true;
+            try
+            {
+                Byte[] DataByte = SodiumHelper.HexToBinary(Data);
+            }
+            catch 
+            {
+                CheckBool = false;
+            }
+            return CheckBool;
+        }
+    }
+}
