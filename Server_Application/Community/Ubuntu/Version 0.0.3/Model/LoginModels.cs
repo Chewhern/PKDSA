86689267d0f746a220c86ede700d2a12abc38f02
Chrono <chrono_legionnaire@protonmail.com>
2023-05-24 14:07:46 +0800

    Create LoginModels.cs

---
+++ Server_Application/Community/Ubuntu/Version 0.0.3/Model/LoginModels.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PKDSA_ServerApp.Model
+{
+    public class LoginModels
+    {
+        public String RequestStatus { get; set; }
+
+        public String SignedRandomChallengeBase64String { get; set; }
+
+        public String ServerECDSAPKBase64String { get; set; }
+    }
+}
