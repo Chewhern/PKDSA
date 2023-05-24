c7b12f7437f50be393f65760d01a465c1a591da4
Chrono <chrono_legionnaire@protonmail.com>
2023-05-24 14:06:32 +0800

    Create MyOwnMySQLConnection.cs

---
+++ Server_Application/Community/Ubuntu/Version 0.0.3/Helper/MyOwnMySQLConnection.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace PKDSA_ServerApp.Helper
+{
+    public class MyOwnMySQLConnection
+    {
+        public MySqlConnection MyMySQLConnection = new MySqlConnection();
+        public Boolean CheckConnection;
+        public String SecretPath = "<Path to MySQL full connection/credential string>";
+        public String ConnectionString = "";
+
+        public void setConnection() {
+            using (StreamReader SecretPathReader = new StreamReader(SecretPath))
+            {
+                while ((ConnectionString = SecretPathReader.ReadLine()) != null)
+                {
+                    MyMySQLConnection.ConnectionString = ConnectionString;
+                }
+            }
+        }
+
+        public Boolean LoadConnection(ref String Exception) {
+            setConnection();
+            try
+            {
+                MyMySQLConnection.Open();
+                CheckConnection = true;
+            }
+            catch (MySqlException exception){
+                CheckConnection = false;
+                Exception = exception.ToString();
+            }
+            return CheckConnection;
+        }
+    }
+}
