1cd9c91f6646d3d89dd365463f33b78d497a2524
Chrono <chrono_legionnaire@protonmail.com>
2023-05-24 13:37:59 +0800

    Add files via upload

---
+++ Management_Applications/Community/Windows/PKDSA_UMWinClient/Version 0.0.3/APIMethodHelper/EstablishConnection.cs
@@ -0,0 +1,82 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using PKDSA_UMWinClient.Helper;
+
+namespace PKDSA_UMWinClient.APIMethodHelper
+{
+    public static class EstablishConnection
+    {
+        public static void CreateLinkWithServer() 
+        {
+            String ServerIP = File.ReadAllText(Application.StartupPath + "\\ServerIP\\IP.txt");
+            Boolean CheckServerOnline = true;
+            if (TORMainOperations.UsingTor == true)
+            {
+                var handler = new HttpClientHandler
+                {
+                    Proxy = new WebProxy(new Uri("socks5://localhost:19050"))
+                };
+
+                using (handler)
+                using (var InitializeHandShakeHttpclient = new HttpClient(handler))
+                {
+                    InitializeHandShakeHttpclient.BaseAddress = new Uri(ServerIP);
+                    InitializeHandShakeHttpclient.DefaultRequestHeaders.Accept.Clear();
+                    InitializeHandShakeHttpclient.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+                    var response = InitializeHandShakeHttpclient.GetAsync("ServerStatus");
+                    try
+                    {
+                        response.Wait();
+                    }
+                    catch
+                    {
+                        CheckServerOnline = false;
+                    }
+                    if (CheckServerOnline == true)
+                    {
+                        APIIPAddressHelper.IPAddress = File.ReadAllText(Application.StartupPath + "\\ServerIP\\IP.txt");
+                        APIIPAddressHelper.HasSet = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("The server is offline now please wait for awhile" + Environment.NewLine +
+                            "You can also change the server IP address if you want to", "Error Message",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else 
+            {
+                using (var InitializeHandShakeHttpclient = new HttpClient())
+                {
+                    InitializeHandShakeHttpclient.BaseAddress = new Uri(ServerIP);
+                    InitializeHandShakeHttpclient.DefaultRequestHeaders.Accept.Clear();
+                    InitializeHandShakeHttpclient.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+                    var response = InitializeHandShakeHttpclient.GetAsync("ServerStatus");
+                    try
+                    {
+                        response.Wait();
+                    }
+                    catch
+                    {
+                        CheckServerOnline = false;
+                    }
+                    if (CheckServerOnline == true)
+                    {
+                        APIIPAddressHelper.IPAddress = File.ReadAllText(Application.StartupPath + "\\ServerIP\\IP.txt");
+                        APIIPAddressHelper.HasSet = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("The server is offline now please wait for awhile" + Environment.NewLine +
+                            "You can also change the server IP address if you want to", "Error Message",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }            
+        }
+
+    }
+}
