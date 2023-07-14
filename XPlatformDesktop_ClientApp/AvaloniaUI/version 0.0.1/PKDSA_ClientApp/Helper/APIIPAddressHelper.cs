52b50751f5ffd30c735a071f5cf0d2cfadf4cd9b
Chrono <chrono_legionnaire@protonmail.com>
2023-07-14 14:56:47 +0800

    Add files via upload

---
+++ XPlatformDesktop_ClientApp/AvaloniaUI/version 0.0.1/PKDSA_ClientApp/Helper/APIIPAddressHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKDSA_ClientApp.Helper
+{
+    public static class APIIPAddressHelper
+    {
+        //Nullable IPAddress
+        public static String? IPAddress { get; set; }
+
+        public static Boolean HasSet { get; set; }
+    }
+}
